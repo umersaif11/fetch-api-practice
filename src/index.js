34664635dd285a0f3e91df9d960a64b3eb04ce01
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-07 04:58:12 -0700

    Use the promise-based fetch api to request the GIPHS from giphy.com api with mode = CORS

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@
 import "./styles.css";
 
 const image =  document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
+    {mode: 'cors'}
+)
