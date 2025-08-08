3dee7b8f28a5564706ccb1c699d6b7d7e150bc87
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-08 02:58:30 -0700

    Since response.json is also the asynchronous promised based so to resolve it and get the final data we need another .then()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const image =  document.querySelector('img');
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
     {mode: 'cors'}
 ).then(
-    function(response){
+    function(response) {
         console.log(response.json);
     }
-);
+).then(
+    function(response) {
+        console.log(response);
+    }
+)
