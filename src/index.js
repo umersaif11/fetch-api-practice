0bdc975012164fcb3bc08fccb5d378220306c3cd
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-08 03:24:05 -0700

    Assign that url path to image.src

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJ
     }
 ).then(
     function(response) {
-        response.data.images.original.url;
+        image.src = response.data.images.original.url;
     }
 )
