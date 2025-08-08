5a98967fae8e9637480c48e099893fdd798a7cdb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-08 02:36:39 -0700

    Add the then method to get the fetch api response in json format

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import "./styles.css";
 const image =  document.querySelector('img');
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
     {mode: 'cors'}
-)
+).then(
+    function(response){
+        console.log(response.json);
+    }
+);
