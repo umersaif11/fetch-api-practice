a55ca06237f43318eb2d2abe2bdd732e5ef910ae
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-12 01:05:27 -0700

    Put the fetch request inside a function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@
 import "./styles.css";
 
 const image =  document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
-    {mode: 'cors'}
-).then(
-    function(response) {
-        return response.json();
-    }
-).then(
-    function(response) {
-        console.log(response);
-        image.src = response.data.images.original.url;
-    }
-)
+const imgButton = document.querySelector('button#new-img');
+
+const getNewImage = () => {
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
+        {mode: 'cors'})
+        .then(function(response) {  
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response);
+            image.src = response.data.images.original.url;
+        })
+}
+getNewImage();
