d300b41b5fc184e82c0b00bda410171da0b395f0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-14 07:19:34 -0700

    Get reference to input, search button and h2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import "./styles.css";
 
 const image =  document.querySelector('img');
 const imgButton = document.querySelector('button#new-img');
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
+const h2 = document.querySelector('h2');
 
 const getNewImage = () => {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats',
+    // const searchParameter = searchInput.value ? encodeURIComponent(searchInput.value) : 'cats';
+    // h2.textContent = (searchInput.value || 'cats') + '@Giphy-API';
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=cats`,
         {mode: 'cors'})
         .then(function(response) {  
             return response.json();
@@ -19,4 +24,5 @@ const getNewImage = () => {
         })
 }
 imgButton.addEventListener('click', getNewImage);
+// searchButton.addEventListener('click', getNewImage)
 getNewImage();
