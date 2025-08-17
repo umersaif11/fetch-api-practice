0c09b17d2143b85141605dde18076a496097c830
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-17 06:12:49 -0700

    Declare an async function get new image, put some safe declaration before the try block

--- src/index.js
+++ src/index.js
@@ -8,28 +8,34 @@ const searchButton = document.querySelector('#search-button');
 const h2 = document.querySelector('h2');
 const loadingPlaceholder = 'https://placehold.co/480x360/282828/D3D3D3?text=Loading...';
 
-const getNewImage = () => {
+// const getNewImage = () => {
+//     image.src = loadingPlaceholder;
+//     const searchParameter = searchInput.value ? encodeURIComponent(searchInput.value) : 'cats';
+//     h2.textContent = (searchInput.value || 'cats') + '@Giphy-API';
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=${searchParameter}`,
+//         {mode: 'cors'})
+//         .then(function(response) {  
+//             return response.json();
+//         })
+//         .then(function(response) {
+//             if(response.data && Object.keys(response.data).length > 0){
+//                 image.src = response.data.images.original.url;
+//             } else {
+//                 image.src = 'https://placehold.co/480x360?text=No+GIF+Found';
+//             }
+//         })
+//         .catch(function(error) {
+//             console.log('Error Fetching:', error);
+//             image.src = 'https://placehold.co/480x360?text=Error';
+//         })
+// }
+async getNewImage = () => {
     image.src = loadingPlaceholder;
     const searchParameter = searchInput.value ? encodeURIComponent(searchInput.value) : 'cats';
     h2.textContent = (searchInput.value || 'cats') + '@Giphy-API';
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Rocr3dF9OJIPffDstJlOTANXJBymd59v&s=${searchParameter}`,
-        {mode: 'cors'})
-        .then(function(response) {  
-            return response.json();
-        })
-        .then(function(response) {
-            if(response.data && Object.keys(response.data).length > 0){
-                image.src = response.data.images.original.url;
-            } else {
-                image.src = 'https://placehold.co/480x360?text=No+GIF+Found';
-            }
-        })
-        .catch(function(error) {
-            console.log('Error Fetching:', error);
-            image.src = 'https://placehold.co/480x360?text=Error';
-        })
 }
 imgButton.addEventListener('click', getNewImage);
 searchButton.addEventListener('click', getNewImage)
 getNewImage();
 
+ 
